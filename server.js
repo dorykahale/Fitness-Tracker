389384ea2820ac0d5b12ec3fe6d8f3fe2f68a3da
DMC <dorykahale@gmail.com>
2021-02-24 21:34:45 -0600

    updated serverJS

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const logger = require("morgan");
-
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -15,6 +13,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { use
 
 app.use(require("./routes/api.js"));
 app.use(require("./routes/views.js"));
+
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 });
